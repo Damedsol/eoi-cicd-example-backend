8f909389748a219a2c5c7e269093f51583116aad
David Leandro Medina Soloza <david.medsol@outlook.es>
2022-07-19 17:57:54 +0100

    Prueba de cambio

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ dotenv.config()
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI}`);
+    res.send(`Estoy ahora mismo en la rama MAIN y tengo esta variable de entorno EOI ${process.env.EOI}
+	esto es una prueba de cambio`);
 });
 
 app.listen(port, () => {
